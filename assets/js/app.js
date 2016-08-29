52ee65848e725ed1bc90c2c12afc660158eabe39
Aaron <newcomer.aaron@icloud.com>
2016-08-28 21:55:31 -0400

    added video functions and templates

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+var wordsmith = angular.module('wordsmith',[]);
+  wordsmith.controller('wordsmithCtlr', function($scope){
+    $scope.formBoxShow = true;
+    $scope.videoShow= false;
+    $scope.tiles = [
+      {title: 'Wordsmith for Spreadsheets',
+       description: "Who doesn't love a good spreadsheet?",
+       linkStyle: 'Learn More >',
+       img: 'assets/excell.png'},
+       {title: 'Ping Pong',
+        description: "Wordsmith promises a 60% higher win rate 100% of the time!",
+        linkStyle: 'Learn More >',
+        img: 'assets/pingPong.png'},
+        {title: 'Zapier',
+         description: "It's not a taser, we promise. It's just awesome.",
+         linkStyle: 'Learn More >',
+         img: 'assets/wordsmith.png'},
+         {title: 'Video Games',
+          description: "Wordsmith plus Mariokart? Oh that is a whole new level!",
+          linkStyle: 'Learn More >',
+          img: 'assets/marioKart.png'}
+
+    ]
+    $scope.videoShowFunc = function(){
+      $scope.formBoxShow = false;
+      $scope.videoShow= true;
+    }
+  })
